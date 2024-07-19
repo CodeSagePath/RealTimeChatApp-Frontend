697a5d2b73382a9c3a5cf6dc9aa7a562398360b4
rishigarg09 <rishigarg185@gmail.com>
2024-07-19 16:51:17 +0530

    removed some redundant files

--- components/ActiveUsers.cy.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ActiveUsers from './ActiveUsers'
-
-describe('<ActiveUsers />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<ActiveUsers />)
-  })
-})
